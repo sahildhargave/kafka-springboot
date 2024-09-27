ef478c3aaece7b9c3a4b7fc29d3411c26dc5c270
Sahildhargave <sahildhargave5253.com>
2024-09-27 12:25:54 +0530

    basic kaffka event driven project

---
+++ base-domains/base-domains/src/test/java/net/javasdworld/base_omains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javasdworld.base_omains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
